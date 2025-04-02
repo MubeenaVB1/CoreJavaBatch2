6634b27260485ef39ccfccbae25cfd5629771a07
MubeenaVB <119161885+MubeenaVB@users.noreply.github.com>
2025-04-02 22:52:49 +0530

    first commit

---
+++ JavaCodesBatch2/src/inheritanceExample/ChildClass1.java
@@ -0,0 +1,9 @@
+package inheritanceExample;
+
+public class ChildClass1 extends ParentClass{
+	
+	public void display() {
+		System.out.println("from child class");
+	}
+
+}
---
+++ JavaCodesBatch2/src/inheritanceExample/ParentClass.java
@@ -0,0 +1,9 @@
+package inheritanceExample;
+
+public class ParentClass {
+	
+	public void print() {
+		System.out.println("From parent class");
+	}
+
+}
