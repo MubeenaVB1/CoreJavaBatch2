02b2d9a0d29c10ebb35dd1ebbf93279ffe74daf6
MubeenaVB <119161885+MubeenaVB@users.noreply.github.com>
2025-04-02 22:58:01 +0530

    first commit

---
+++ JavaCodesBatch2/src/inheritanceExample/MainClass.java
@@ -0,0 +1,13 @@
+package inheritanceExample;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		ChildClass1 c = new ChildClass1();
+		c.display();
+		c.print();
+
+	}
+
+}
